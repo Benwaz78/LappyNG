b81f2b4248ae77d5666802b83017644a10cbf3bd
Benwaz78 <nonwaz78@gmail.com>
2021-10-24 10:01:26 -0400

    Saved

--- lappyng_project/settings.py
+++ lappyng_project/settings.py
@@ -216,7 +216,7 @@ JET_APP_INDEX_DASHBOARD = 'jet.dashboard.dashboard.DefaultAppIndexDashboard'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'https://lappy.ng/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
