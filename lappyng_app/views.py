78157dc85d0f9ac2f4aa5ff39158876a38fe951b
abidemi-mina <aminatabidemi212@gmail.com>
2021-11-13 18:20:34 +0100

    'aminat'

--- lappyng_app/views.py
+++ lappyng_app/views.py
@@ -46,7 +46,7 @@ class Home(TemplateView):
         context['products'] = Products.objects.all()
         context['abt'] = About.objects.all()
         context['top_banner1'] = HomeTopBanner.objects.first()
-        context['top_banner2'] = HomeTopBanner.objects.all()[1]
+        context['top_banner2'] = HomeTopBanner.objects.all()[:1]
         context['two_side_banner'] = HomeTwoSideBanner.objects.all()[:2]
         context['sidebar_banner'] = HomeSideBanner.objects.first()
         context['lenovo'] = Products.objects.filter(brand__id=4)[:5]
